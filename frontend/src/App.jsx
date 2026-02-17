c2394e62bb19f21e63873169291d3210da51f2ba
deanmacdonald <deanmacdonald96@msn.com>
2026-02-17 14:18:44 -0700

    cleanup

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import GameScreen from "./screens/GameScreen.jsx";
-
-export default function App() {
-    return (
-        <div style={{ width: "100%", height: "100%" }}>
-            <GameScreen />
-        </div>
-    );
-}
